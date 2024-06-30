7a5a6a4b48351ff3aa44f62302dd441a20cf1dd2
cody-berry <cody.tian@icloud.com>
2024-06-29 19:10:03 -0700

    Make a special case for lanthanides and actinides

--- sketch.js
+++ sketch.js
@@ -9,17 +9,19 @@ let fixedWidthFont
 let variableWidthFont
 let instructions
 let debugCorner /* output debug text in the bottom left corner of the canvas */
+let periodicTableJSON
 
 
 function preload() {
     font = loadFont('data/consola.ttf')
     fixedWidthFont = loadFont('data/consola.ttf')
     variableWidthFont = loadFont('data/meiryo.ttf')
+    periodicTableJSON = loadJSON('data/elementsBowserinator.json')
 }
 
 
 function setup() {
-    let cnv = createCanvas(1000, 700)
+    let cnv = createCanvas(1500, 900)
     cnv.parent('#canvas')
     colorMode(HSB, 360, 100, 100, 100)
     textFont(font, 14)
@@ -37,7 +39,88 @@ function setup() {
 
 function draw() {
     background(234, 34, 24)
+    fill(0, 0, 100)
 
+    // display all period labels
+    // there are 7 periods. However, since it's been around 20 years since
+    // Og (element 118, the last element on most periodic tables and the
+    // last element of period 7) was discovered, so I'm expecting ununseptium
+    // to be named soon, which will add an 8th period. Go scientists!!!
+    // Although it's entirely possible there's a group 19 or something, and
+    // I totally do not expect that. But I'm no expert on chemistry, and I
+    // don't think even the experts know, because I believe it's impossible to
+    // know a lot about undiscovered elements.
+
+    let elementSize = 75 // each element is a square. this is the size of it.
+
+    let yPos = elementSize*3/2 // we are displaying this in the center.
+    let xPos = elementSize*3/4 // the xPos to display the period labels on.
+    textAlign(CENTER, CENTER)
+    for (let period = 1; period <= 7; period += 1) {
+        text(period, xPos, yPos)
+        yPos += elementSize
+    }
+
+    // on the other hand, the groups are a bit different. we're going to be
+    // displaying them on varying yPoses.
+    // we're representing on which square on the virtual grid we're
+    // displaying them on.
+    let yPoses = [1, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 1]
+    xPos = elementSize*3/2
+    for (let group = 1; group <= 18; group += 1) {
+        text(group, xPos, (yPoses[group - 1] - 1/4)*elementSize)
+        xPos += elementSize
+    }
+
+    // now we display a square for each element
+    for (let element of periodicTableJSON["elements"]) {
+        // xPos and yPos represent the top-left corner of the grey square
+        // note: there is a padding of 4, making for a grey square that is
+        // actually 68 in size.
+        xPos = element["group"]*elementSize
+        yPos = element["period"]*elementSize
+
+        // however, there is an exception with the lanthanides and actinides.
+        // the json displays it as period 6 or 7 and group 3, whereas it's
+        // actually supposed to be displayed as if it's period 8 or 9 and group
+        // 4+.
+
+        // atomic numbers 57-71 correspond to lanthanides (groups 4-18).
+        if (element["number"] >= 57 && element["number"] <= 71) {
+            let group = element["number"] - 53
+            let period = element["period"] + 2
+            xPos = group*elementSize
+            yPos = period*elementSize
+        }
+
+        // atomic numbers 89-103 correspond to actinides (groups 4-18).
+        if (element["number"] >= 89 && element["number"] <= 103) {
+            let group = element["number"] - 85
+            let period = element["period"] + 2
+            xPos = group*elementSize
+            yPos = period*elementSize
+        }
+
+
+
+        fill(0, 0, 25, 100)
+        rectMode(CORNER)
+        square(xPos + 4, yPos + 4, elementSize - 8)
+    }
+
+    // draw the parrellelegram between the last two alkaline earth metals (at
+    // least, until the eight alkaline earth metal is releast. It should be
+    // element 120, but who knows?)
+    // we have an increased y padding (6); otherwise it looks weird
+    fill(0, 0, 25, 100)
+    quad(3*elementSize + 4, 7*elementSize - 6,
+         3*elementSize + 4, 6*elementSize + 6,
+         4*elementSize - 4, 8*elementSize + 6,
+         4*elementSize - 4, 9*elementSize - 6)
+    quad(3*elementSize + 4, 8*elementSize - 6,
+         3*elementSize + 4, 7*elementSize + 6,
+         4*elementSize - 4, 9*elementSize + 6,
+         4*elementSize - 4, 10*elementSize - 6)
 
 
     /* debugCorner needs to be last so its z-index is highest */
